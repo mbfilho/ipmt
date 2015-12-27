7d9818ede1126e23818f9182e2d7bfc4c2f6fdc6
Márcio Barbosa <mbfilho91@gmail.com>
2015-12-26 21:39:23 -0300

    Adicionei o findPredecessor no array de sufixo

--- src/indexes/SuffixArray.cpp
+++ src/indexes/SuffixArray.cpp
@@ -186,6 +186,40 @@ int SuffixArray::findSuccessor(const char* pat, size_t m) {
 	return r;
 }
 
+/*
+* Retorna a última posição do array de sufixo que possui um sufixo lexicograficamente <=_m 'pat'
+*/
+int SuffixArray::findPredecessor(const char* pat, size_t m) {
+	int L = getLcp(pat, text + suffixArray[0]), R = getLcp(pat, text + suffixArray[n-1]);
+
+	if(L < m && (suffixArray[0]+L < n) && pat[L] < text[suffixArray[0]+L])//Trata o caso em que o predecessor não existe: todos os sufixos são maiores que 'pat'
+		return -1;
+	if(R == m || suffixArray[n-1] + R >= n ||pat[R] >= text[suffixArray[n-1] + R])//Verifica se o último sufixo <=_m pat e, portanto, é a resposta
+		return n-1;
+
+	int l = 0, r = n-1;
+	//[l,r)	
+
+	while(r-l>1) {
+		int h = (r+l)>>1, H;	
+
+		if(L >= R) {
+			if(lLcp[h] < L) H = lLcp[h];
+			else H = L + getLcp(pat + L, text + suffixArray[h] + L);	
+		} else {
+			if(rLcp[h] < R) H = rLcp[h];
+			else H = R + getLcp(pat + R, text + suffixArray[h] + R);
+		}
+
+		if(H == m || suffixArray[h]+H >= n || text[suffixArray[h]+H] <= pat[H]) //o sufixo em sa[h] eh <=_m pat. Ele pode ser a resposta.
+			l = h, L = H;
+		else
+			r = h, R = H;	
+	}
+
+	return l;
+}
+
 
 
 

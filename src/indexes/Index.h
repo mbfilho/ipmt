1525040ec2c2dcb021f654bac3c90fa4e6c6c14c
Márcio Barbosa <mbfilho91@gmail.com>
2015-12-27 22:34:06 -0300

    Faltou esse arquivo para a adaptação dos indices à compressão

--- src/indexes/Index.h
+++ src/indexes/Index.h
@@ -1,6 +1,8 @@
 #ifndef INDEX
 #define INDEX
 #include <cstdlib>
+#include "Compressor.h"
+#include "Decompressor.h"
 
 /*
  * A classe mãe de todos os indexes
@@ -9,5 +11,7 @@ class Index {
 	public:
 	virtual void build(const char* text, size_t n) = 0;
 	virtual void findMatchings(const char* pat, size_t m, bool countOnly) = 0; 
+	virtual void compress(Compressor* compressor) = 0; 
+	virtual void decompress(Decompressor* decompressor) = 0; 
 };
 #endif

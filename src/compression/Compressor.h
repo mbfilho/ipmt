a86da0b4aeb5271c2b112d0669924def3113734f
Márcio Barbosa <mbfilho91@gmail.com>
2015-12-27 22:32:15 -0300

    Correções nos compressores/decompressores

--- src/compression/Compressor.h
+++ src/compression/Compressor.h
@@ -1,6 +1,11 @@
+#ifndef COMPRESSOR_H
+#define COMPRESSOR_H
+
 class Compressor {
 public:
 	virtual void writeInt(int arg) = 0;
 	virtual void writeByte(char arg) = 0;
 	virtual void flushAndClose() = 0;
 };
+
+#endif
--- src/compression/Decompressor.h
+++ src/compression/Decompressor.h
@@ -1,6 +1,11 @@
+#ifndef DECOMPRESSOR_H
+#define DECOMPRESSOR_H
+
 class Decompressor {
 public:
 	virtual int readInt() = 0;
 	virtual char readByte() = 0;
 	virtual void close() = 0;
 };
+
+#endif

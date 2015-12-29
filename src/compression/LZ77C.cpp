83f0b23c312825e7a0d77b9aa368a57b3b86161e
Márcio Barbosa <mbfilho91@gmail.com>
2015-12-29 20:55:20 -0300

    Continuação da refatoração e correção do LZ77C

--- src/compression/LZ77C.cpp
+++ src/compression/LZ77C.cpp
@@ -1,16 +1,14 @@
 #include "LZ77C.h"
 
-LZ77C::LZ77C(const char* fileName, int bufferSize, int lookAheadSize) : window(bufferSize + lookAheadSize){
-	assert(bufferSize + lookAheadSize <= (1<<23));
-	file = fopen(fileName, "wb");
-	
-	if(!file) {
-		printf("[LZ77 compressor] Não foi possível abrir o arquivo \'%s\' para escrita.\n", fileName);
-		assert(file);
-	}
-
+LZ77C::LZ77C(const char* fileName, int bufferSize, int lookAheadSize) : window(bufferSize + lookAheadSize), Compressor(fileName) {
 	WB = bufferSize;
 	WL = lookAheadSize;
+
+	bitsWB  = (WB == 0 ? 1 : 32 - __builtin_clz(WB));
+	bitsWL  = (WL == 0 ? 1 : 32 - __builtin_clz(WL));
+	
+	assert(bitsWB + bitsWL + 8 <= 64);
+
 	fail = new int[WL];
 
 	/*
@@ -21,26 +19,6 @@ LZ77C::LZ77C(const char* fileName, int bufferSize, int lookAheadSize) : window(b
 	for(int i = 0; i < WB; ++i)
 		window.append(0);
 
-	outputBuffer = new ull[MAX_OUTPUTBUFFER_SIZE + 1];
-	outSize = 0;
-}
-
-void LZ77C::writeInt(int arg) {
-	ui u = ui(arg);
-	for(int i = 0; i < 4; ++i) {
-		writeByte(u & 0xff);	
-		u >>= 8;
-	}
-}
-
-void LZ77C::writeArrayOfInts(const int* arg, int size) {
-	for(int i = 0; i < size; ++i)
-		writeInt(arg[i]);
-}
-
-void LZ77C::writeText(const char* text, int size) {
-	for(int i = 0; i < size; ++i)
-		writeByte(text[i]);
 }
 
 void LZ77C::writeByte(int arg) {
@@ -69,6 +47,14 @@ void LZ77C::emmitToken() {
 	int k = -1;
 	ui bestMatchingSize = 0, bestMatchingPos;
 	int wl = window.getSize() - WB ;
+
+	/*Investigar melhor maneira de fazer isso.
+	* Garante que sempre vai haver um caractere de mismatching.
+	* Quando wl != WL, eh o final da stream, e o último caracter não está no alfabeto.
+	*/
+	if(wl == WL) 
+		--wl;
+
 	for(int i = 0; i < window.getSize(); ++i){
 		int cur = window.get(i);
 		while(k != -1 && window.get(WB + k+1) != cur)
@@ -95,31 +81,18 @@ void LZ77C::emmitToken() {
 
 	ull token = 
 		bestMatchingPos
-		| (ull(bestMatchingSize) << 23)
-		| (ull(ui(mismatchingChar)) << 56);
-	insertIntoBuffer(token);
-	
+		| (ull(bestMatchingSize) << (bitsWB))
+		| (ull(ui(mismatchingChar)) << (bitsWB+bitsWL));
+	insertIntoBuffer(token, 8 + bitsWB + bitsWL);
+	printf("(%u, %u, %u) Size %d\n", bestMatchingPos, bestMatchingSize, mismatchingChar, 8+bitsWB+bitsWL);
 	window.slide(bestMatchingSize+1);
 }
 
 void LZ77C::flushAndClose() {
 	while(window.getSize() > WB) 
 		emmitToken();
-
-	if(outSize)  {
-		assert(outSize == fwrite(outputBuffer, sizeof(ull), outSize, file));
-		outSize = 0;
-	}
-
-	fclose(file);
+	
+	flush(true);
+	close();
 }
-
-void LZ77C::insertIntoBuffer(ull token) {
-	outputBuffer[outSize++] = token;
-	if(outSize >= MAX_OUTPUTBUFFER_SIZE)  {
-		assert(outSize == fwrite(outputBuffer, sizeof(ull), outSize, file));
-		outSize = 0;
-	}
-}
-
 
--- src/compression/LZ77C.h
+++ src/compression/LZ77C.h
@@ -4,30 +4,23 @@
 #include "SlidingWindow.h"
 #include "Global.h"
 
-#define MAX_OUTPUTBUFFER_SIZE 1024
-
 class LZ77C : public Compressor {
 public:
 	LZ77C(const char* fileName, int bufferSize, int lookAheadSize);
 	
-	void writeInt(int arg);
-	void writeArrayOfInts(const int* arg, int size);
 	void writeByte(int arg);	
-	void writeText(const char* text, int size);
 
 	void flushAndClose();
 
 private:
 	SlidingWindow window;
 	int WB, WL; //os tamanhos das janelas de buffer (WB) e da janela 'lookahead' (WL)
+	int bitsWB, bitsWL; //A quantidade de bits para se representar WB e WL, respectivamente.
+
 	int* fail; //a função de falha do KMP para WL
 	FILE* file;
 
-	ull* outputBuffer;
-	int outSize;
-	
 	void emmitToken();
-	void insertIntoBuffer(ull token);
 	void buildFailFunction();
 };
 

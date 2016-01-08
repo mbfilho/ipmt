c908152bfe018e1716551ca56431edb59054e37c
Márcio Barbosa <mbfilho91@gmail.com>
2016-01-08 17:25:08 -0300

    Resultados dos testes com suffixtree2

--- src/bench/build.py
+++ src/bench/build.py
@@ -1,9 +1,10 @@
 import subprocess
 import os
 
-textFiles = ['1mb', '2mb', '3mb', '5mb', '6mb', '7mb', '8mb', '9mb', '10mb', '20mb', '30mb', '40mb', '50mb']
-repetitions = 3
-numberOfFiles = 3
+#textFiles = ['1mb', '2mb', '3mb', '5mb', '6mb', '7mb', '8mb', '9mb', '10mb', '20mb', '30mb', '40mb', '50mb']
+textFiles = ['90mb']
+repetitions = 1
+numberOfFiles = 1
 
 def getSize(fileName):
 	info = os.stat(fileName)
@@ -33,16 +34,16 @@ def executeAndGetTime(line):
 		print out
 	return t
 
-timeFile = open('time.results', 'w')
-sizeFile = open('size.results', 'w')
-headers = ['FileSize', 'Arvore de Sufixos', 'Array de Sufixos']
-timeFile.write( (3 * '%20s') % tuple(headers))
-timeFile.write('\n')
-sizeFile.write( (3 * '%20s') % tuple(headers))
-sizeFile.write('\n')
+timeFile = open('time.results', 'a')
+sizeFile = open('size.results', 'a')
+headers = ['FileSize', 'Arvore de Sufixos', 'Array de Sufixos', 'Arvore de Sufixos 2']
+#timeFile.write( (4 * '%20s') % tuple(headers))
+#timeFile.write('\n')
+#sizeFile.write( (3 * '%20s') % tuple(headers[:-1]))
+#sizeFile.write('\n')
 
 for aux in textFiles:
-	timeResults = 2 * [0]
+	timeResults = 3 * [0]
 	sizeResults = 2 * [0]
 
 	for f in range(numberOfFiles):
@@ -52,21 +53,22 @@ for aux in textFiles:
 		executeAndGetTime(cmdline % (text, 1, 'suffixtree'))
 		sizeResults[0] += getSize(text + '.idx')
 		
-		executeAndGetTime(cmdline % (text, 1, 'suffixarray'))
-		sizeResults[1] += getSize(text + '.idx')
+		#executeAndGetTime(cmdline % (text, 1, 'suffixarray'))
+		#sizeResults[1] += getSize(text + '.idx')
 
 		originalFileSize = getSize(text)
 
 		for r in range(repetitions):
 			timeResults[0] += executeAndGetTime(cmdline % (text, 0, 'suffixtree'))
-			timeResults[1] += executeAndGetTime(cmdline % (text, 0, 'suffixarray'))
+	#		timeResults[1] += executeAndGetTime(cmdline % (text, 0, 'suffixarray'))
+			timeResults[2] += executeAndGetTime(cmdline % (text, 0, 'suffixtree2'))
 
 	timeLine = [originalFileSize]
 
 	for value in timeResults:
 		timeLine += [value / float(repetitions*numberOfFiles)]
 	
-	timeFile.write( (3 * '%20.3lf') % tuple(timeLine))
+	timeFile.write( (4 * '%20.3lf') % tuple(timeLine))
 	timeFile.write('\n')
 	timeFile.flush();
 
